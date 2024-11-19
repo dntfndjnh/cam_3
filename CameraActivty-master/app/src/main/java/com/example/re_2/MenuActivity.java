8c10c436e1b722d44fcd2f1af9813fe73de33a77
Robbie <Robbie@email.com>
2024-11-19 11:14:11 +0900

    make new activity

---
+++ CameraActivty-master/app/src/main/java/com/example/re_2/MenuActivity.java
@@ -0,0 +1,33 @@
+package com.example.re_2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_menu);
+
+        String bitmap_string= getIntent().getStringExtra("bitmap_string");
+        //Intent intent = getIntent();
+
+
+        Log.v("bitmap=",".."+bitmap_string);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
